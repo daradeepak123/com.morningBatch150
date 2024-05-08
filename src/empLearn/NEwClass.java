0601a924a24403dc1f30e892bef3e01117d45e8f
Deepak <102788188+daradeepak123@users.noreply.github.com>
2024-05-08 09:17:01 +0530

    This is simple sysOut

--- src/empLearn/NEwClass.java
+++ src/empLearn/NEwClass.java
@@ -3,8 +3,10 @@ package empLearn;
 public class NEwClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("THis is hello");
+		
+		
 	}
 
 }
